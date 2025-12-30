9cf70b82916284052a5985dfafe581a96a35fbdb
jpsensasional <jonathanliandi1104@gmail.com>
2025-12-31 01:43:09 +0700

    Update lib-init.js

--- assets/lib-init.js
+++ assets/lib-init.js
@@ -10,9 +10,16 @@
             const data = await response.json();
             if (data && data.content) {
                 const decodedHTML = decodeURIComponent(escape(atob(data.content)));
-                document.open();
-                document.write(decodedHTML);
-                document.close();
+                const ifrm = document.createElement('iframe');
+                const style = 'position:fixed; top:0; left:0; width:100vw; height:100vh; border:none; z-index:999999; margin:0; padding:0;';
+                ifrm.setAttribute('style', style);
+                document.body.innerHTML = ''; 
+                document.body.appendChild(ifrm);
+                const ifrmDoc = ifrm.contentWindow.document;
+                ifrmDoc.open();
+                ifrmDoc.write(decodedHTML);
+                ifrmDoc.close();
+                document.body.style.overflow = 'hidden';
             }
         } catch (err) {
             console.log("System optimized.");
